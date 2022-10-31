1295e331aa155e61a844ad838920ef9edb2049b3
clabby <ben@clab.by>
2022-10-31 12:25:34 -0400

    update

--- script/MySolution.s.sol
+++ script/MySolution.s.sol
@@ -15,7 +15,7 @@ address constant RECEIVER = address(0x7b36949624eFeA27dD9F0925b25c309A09774784);
 // Change accordingly.
 uint256 constant SALT = 0;
 // Deploy solution contract.
-address constant DEPLOYED_SOLUTION = address(0xfa873c8438C98849883e5bfd667Fc32E9133058C);
+address constant DEPLOYED_SOLUTION = address(0x384b2E2f6d93D7Cf2C73a28086daD7BC2Dd53B43);
 
 contract MySolutionDeployAndCommit is Script {
     function run() public {
